562d8294e5f24d8dcf78754f4b17439d61fc8d81
Guiee <466007639@qq.com>
2020-02-19 13:52:52 +0800

    data

---
+++ flink-train/src/main/java/flink/demo/batch/BatchWCApp.java
@@ -0,0 +1,28 @@
+package flink.demo.batch;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.util.Collector;
+
+
+public class BatchWCApp {
+    public static void main(String[] args) throws Exception {
+        String input = "file:///Users/apuuu/data";
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        DataSource<String> text = env.readTextFile(input);
+        //        text.print();
+        text.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+            @Override
+            public void flatMap(String value, Collector<Tuple2<String, Integer>> collector) throws Exception {
+                String[] tokens = value.toLowerCase().split(" ");
+                for (String token : tokens) {
+                    if (token.length() > 0) {
+                        collector.collect(new Tuple2<>(token, 1));
+                    }
+                }
+            }
+        }).groupBy(0).sum(1).print();
+    }
+}
